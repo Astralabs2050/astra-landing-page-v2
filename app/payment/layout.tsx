75904e32723f9b3bcc4d1406e20e846a5a3d9c3c
Ijeoma Igbokwe <129294448+ijay123@users.noreply.github.com>
2024-07-01 21:01:57 +0100

    feat: updated payment page

--- app/payment/layout.tsx
+++ app/payment/layout.tsx
@@ -2,10 +2,10 @@ import React, { PropsWithChildren } from 'react'
 import { Header } from '@/components/common'
 import { redirect } from 'next/navigation'
 import { getUser } from '@/server/actions/user'
-import Payment from './page'
+import { Payment } from './payment'
 
 // Define the props type
-interface PaymentLayoutProps extends PropsWithChildren {
+interface PaymentLayoutProps {
   brand: React.ReactNode
   creator: React.ReactNode
 }
@@ -34,3 +34,28 @@ export default async function PaymentLayout({
     </div>
   )
 }
+
+// export default async function PaymentLayout({
+//   brand,
+//   creator,
+// }: PaymentLayoutProps) {
+//   const user = await getUser()
+
+//   if (!user?.onboarded) {
+//     return redirect('/')
+//   }
+
+//   return (
+//     <div className="flex w-screen flex-col">
+//       <main className="flex min-h-screen w-full grow flex-col">
+//         <Header user={user} />
+//         <div className="m-auto flex items-center justify-center">
+//           <Payment user={user} />
+//           <div className="border-l border-solid border-gray-6 p-10">
+//             {user?.role === 'BRAND' ? brand : creator}
+//           </div>
+//         </div>
+//       </main>
+//     </div>
+//   )
+// }
